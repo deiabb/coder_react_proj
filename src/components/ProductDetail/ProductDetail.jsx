6e8045b7325502b9665f7883edade3824f3946fa
Deborah B Barra <deborahbbarra@gmail.com>
2024-10-21 20:22:06 -0300

    Implementação do componente Product Detail

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import styles from "./ProductDetail.module.css";
 import { useParams } from "react-router-dom";
+import ProductCard from "../ProductCard/ProductCard";
 
 export default function ProductDetail() {
   const { id } = useParams();
   const [product, setProduct] = useState({});
   
   useEffect(() => {
-    fetch(`https://api.mercadolibre.com/sites/MLB/search?q=papelaria/${id}`)
+    fetch(`https://api.mercadolibre.com/items/${id}`)
     .then((response) => response.json())
     .then((data) => {
         setProduct(data)
@@ -16,10 +17,14 @@ export default function ProductDetail() {
   console.log(product)
   //isso funciona, ele me devolve um array de objetos, mas não estou conseguindo acessar o título 
 
+  
+
   return (
     <div>
-      <h1>{product.query}</h1>
+      <img src={product.thumbnail}/>
+      <h3>{product.title}</h3>
+      <h4>R$ {product.price}</h4>
       
-      </div>
+    </div>
   );
 }
